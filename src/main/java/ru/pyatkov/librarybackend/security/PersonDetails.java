3f271fe57edfa4af5b03d435a81e461b7394c765
Sergey Pyatkov <spyatkov@smart-ts.ru>
2024-03-22 17:12:46 +0500

    #swagger - Пока убрал секьюрити, добавил сваггер

--- src/main/java/ru/pyatkov/librarybackend/security/PersonDetails.java
+++ src/main/java/ru/pyatkov/librarybackend/security/PersonDetails.java
@@ -1,53 +1,53 @@
-package ru.pyatkov.librarybackend.security;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import ru.pyatkov.librarybackend.models.Person;
-
-import java.util.Collection;
-import java.util.List;
-
-public class PersonDetails implements UserDetails {
-
-    private final Person person;
-
-    public PersonDetails(Person person) {
-        this.person = person;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("user"));
-    }
-
-    @Override
-    public String getPassword() {
-        return this.person.getPassword();
-    }
-
-    @Override
-    public String getUsername() {
-        return this.person.getUsername();
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
-}
+//package ru.pyatkov.librarybackend.security;
+//
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import ru.pyatkov.librarybackend.models.Person;
+//
+//import java.util.Collection;
+//import java.util.List;
+//
+//public class PersonDetails implements UserDetails {
+//
+//    private final Person person;
+//
+//    public PersonDetails(Person person) {
+//        this.person = person;
+//    }
+//
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return List.of(new SimpleGrantedAuthority("user"));
+//    }
+//
+//    @Override
+//    public String getPassword() {
+//        return this.person.getPassword();
+//    }
+//
+//    @Override
+//    public String getUsername() {
+//        return this.person.getUsername();
+//    }
+//
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//        return true;
+//    }
+//}
